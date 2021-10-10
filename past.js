e21dd74614301c41303c030abfc47df5cfa561de
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-10 18:27:40 +0530

    Update past.js

--- past.js
+++ past.js
@@ -2,7 +2,7 @@ export const past=[
      {
         "siteName": "Day 6",
         "siteURL": "https://www.youtube.com/watch?v=n8Mk31j5Rxw",
-        "siteImage": "/Day6.png",
+        "siteImage": "/day6.png",
         "siteDesc": "Softnotions Technologies Pvt Ltd",
         "eventTime":"Thu Oct 09 2021 17:00:00 GMT+0530 (India Standard Time)"
     },
