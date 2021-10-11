5c2696c4fc4939204b090fc94c37817c437b3365
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-11 18:11:04 +0530

    Update today.js

--- today.js
+++ today.js
@@ -1,9 +1,9 @@
 export const today = [
     {
-        "siteName": "Day 7",
-        "siteURL": "https://www.youtube.com/watch?v=ssqerqGDluo",
-        "siteImage": "/day7.png",
-        "siteDesc": "Triassic Solutions Private Limited",
-        "eventTime":"Sunday Oct 10 2021 19:00:00 GMT+0530 (India Standard Time)"
+        "siteName": "Day 8",
+        "siteURL": "https://www.youtube.com/watch?v=vbN5GSZBT1A",
+        "siteImage": "/Day8.png",
+        "siteDesc": "Experion Technologies Pvt Ltd",
+        "eventTime":"Sunday Oct 11 2021 19:00:00 GMT+0530 (India Standard Time)"
     },
 ]
