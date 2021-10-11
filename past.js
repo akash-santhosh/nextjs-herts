c2a336b5a43ba57ad21869ed236c578f7a413802
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-11 18:07:12 +0530

    Update past.js

--- past.js
+++ past.js
@@ -1,5 +1,12 @@
 export const past=[
      {
+        "siteName": "Day 7",
+        "siteURL": "https://www.youtube.com/watch?v=ssqerqGDluo",
+        "siteImage": "/day7.png",
+        "siteDesc": "Triassic Solutions Private Limited",
+        "eventTime":"Sunday Oct 10 2021 19:00:00 GMT+0530 (India Standard Time)"
+    },
+     {
         "siteName": "Day 6",
         "siteURL": "https://www.youtube.com/watch?v=n8Mk31j5Rxw",
         "siteImage": "/day6.png",
