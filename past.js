1d81e828316a3fd05daa80c00e31357b3e708dbc
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-12 18:36:29 +0530

    Update past.js

--- past.js
+++ past.js
@@ -1,5 +1,12 @@
 export const past=[
      {
+        "siteName": "Day 8",
+        "siteURL": "https://www.youtube.com/watch?v=vbN5GSZBT1A",
+        "siteImage": "/Day8.png",
+        "siteDesc": "Experion Technologies Pvt Ltd",
+        "eventTime":"Sunday Oct 11 2021 19:00:00 GMT+0530 (India Standard Time)"
+     },
+     {
         "siteName": "Day 7",
         "siteURL": "https://www.youtube.com/watch?v=ssqerqGDluo",
         "siteImage": "/Day7.png",
