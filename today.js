2a4a658c969744c433e2b080c518218797612218
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-11-16 20:29:16 +0530

    Update today.js

--- today.js
+++ today.js
@@ -1,9 +1,9 @@
 export const today = [
     {
-        "siteName": "Day 9",
+        "siteName": "Day 10",
         "siteURL": "https://www.youtube.com/watch?v=j4zfHr8ZSV8",
-        "siteImage": "/Day9.png",
-        "siteDesc": "Travanleo Info Solutions Pvt. Ltd",
-        "eventTime":"Sunday Oct 12 2021 19:00:00 GMT+0530 (India Standard Time)"
+        "siteImage": "/Day10.png",
+        "siteDesc": "Thinkpalm Technologies Pvt. Ltd.",
+        "eventTime":"Wednesday 2021 19:00:00 GMT+0530 (India Standard Time)"
     }
 ]
