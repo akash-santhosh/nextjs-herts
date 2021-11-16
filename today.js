19d3ba1bbf6b2ed2b7525fbda6df12de76a4ccf5
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-11-16 20:25:59 +0530

    Update today.js

--- today.js
+++ today.js
@@ -5,5 +5,5 @@ export const today = [
         "siteImage": "/Day9.png",
         "siteDesc": "Travanleo Info Solutions Pvt. Ltd",
         "eventTime":"Sunday Oct 12 2021 19:00:00 GMT+0530 (India Standard Time)"
-    },
+    }
 ]
