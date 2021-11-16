3c6a91b5293ee98af9c330524b10886e5996a5b4
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-11-16 20:32:27 +0530

    Update past.js

--- past.js
+++ past.js
@@ -1,4 +1,11 @@
 export const past=[
+       {
+        "siteName": "Day 9",
+        "siteURL": "https://www.youtube.com/watch?v=j4zfHr8ZSV8",
+        "siteImage": "/Day9.png",
+        "siteDesc": "Travanleo Info Solutions Pvt. Ltd",
+        "eventTime":"Sunday Oct 12 2021 19:00:00 GMT+0530 (India Standard Time)"
+     },
      {
         "siteName": "Day 8",
         "siteURL": "https://www.youtube.com/watch?v=vbN5GSZBT1A",
